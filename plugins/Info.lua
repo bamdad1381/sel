190b04c128e7fbfc9918d5e7f47d7943b71d1692
bamdad1381 <bamdadnikkhah@gmail.com>
2016-05-18 17:59:17 +0430

    Create Info.lua

---
+++ plugins/Info.lua
@@ -0,0 +1,14 @@
+do
+
+function run(msg, matches)
+  return "#Group_ID: "..msg.to.id.."\n#Group_Name: "..msg.to.title.."\n#Your_Name: "..(msg.from.first_name or '').."\n#First_Name: "..(msg.from.first_name or '').."\n#Last_Name: "..(msg.from.last_name or '').."\n#ID: "..msg.from.id.."\n#UserName: @"..(msg.from.username or '').."\n#Phone_Number: +"..(msg.from.phone or '')
+end
+return {
+  description = "", 
+  usage = "",
+  patterns = {
+    "^[!/#][Ii]nfo",
+  },
+  run = run
+}
+end
