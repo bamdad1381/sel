425747dcc2888523249ea6c67b476123bff1e12f
bamdad1381 <bamdadnikkhah@gmail.com>
2016-05-18 19:01:45 +0430

    Create Time.lua

---
+++ plugins/Time.lua
@@ -0,0 +1,25 @@
+--[[
+#
+#    Time And Date
+#
+#    @Dragon_born
+#	@GPMod
+#
+#
+]]
+
+function run(msg, matches)
+local url , res = http.request('http://api.gpmod.ir/time/')
+if res ~= 200 then return "No connection" end
+local jdat = json:decode(url)
+local text = '🕒FA Time:\n'..jdat.FAtime..' \n📆FA Date:\n'..jdat.FAdate..'\n🕒EN Time:\n'..jdat.ENtime..'\n📆EN Date:\n '..jdat.ENdate.. '\n---------------\n@Xx_Epicure_xX
+return text
+end
+return {
+  patterns = {
+  "^[/#!]([Tt][iI][Mm][Ee])$"
+  }, 
+run = run 
+}
+
+
