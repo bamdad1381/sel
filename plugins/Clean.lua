bd9852e25a032c766523b6275c06d1004d5dbe2e
bamdad1381 <bamdadnikkhah@gmail.com>
2016-05-18 13:39:32 +0430

    Update and rename Spammer1.lua to Clean.lua

--- plugins/Clean.lua
+++ plugins/Clean.lua
@@ -2,10 +2,10 @@ do
 
 function run(msg, matches)
        if not is_sudo(msg) then
-              return "Only Sudo Can Use It!"       end
+              return "/!\Only Sudo Can Use It/!\"       end
   local tex = matches[1]
   local sps = matches[2]
-  local sp = 'done by DaedTM'
+  local sp = matches[3]
 
 for i=1, tex, 1 do
 
@@ -20,7 +20,7 @@ end
 
 return {
     patterns = {
-      "[!/#]spam (.*) (.*)$"
+      "[!/#]clean (.*) (.*)$"
     },
     run = run,
 }
