bdee259435aeb2852daeece396e4df88c92319b8
bamdad1381 <bamdadnikkhah@gmail.com>
2016-05-19 23:29:04 +0430

    Create Aparat.lua

---
+++ plugins/Aparat.lua
@@ -0,0 +1,22 @@
+
+local function run(msg, matches)
+	if matches[1]:lower() == 'aparat' then
+		local url = http.request('http://www.aparat.com/etc/api/videoBySearch/text/'..URL.escape(matches[2]))
+		local jdat = json:decode(url)
+
+		local items = jdat.videobysearch
+		text = 'Search Result: \n'
+		for i = 1, #items do
+		text = text..'\n'..i..'Name: '..items[i].title..'\n Visit Count: '..items[i].visit_cnt..'\n Link: aparat.com/v/'..items[i].uid
+		end
+		text = text..'\n---------------\n@Xx_Epicure_xX'
+		return text
+	end
+end
+
+return {
+   patterns = {
+"^[/#!](aparat) (.*)$",
+   },
+   run = run
+}
