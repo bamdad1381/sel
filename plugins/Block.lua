12a0ad3cb9e5f9e2e2098499fe8e967dd919e82f
bamdad1381 <bamdadnikkhah@gmail.com>
2016-05-21 13:13:36 +0430

    Update Block.lua

--- plugins/Block.lua
+++ plugins/Block.lua
@@ -2,11 +2,11 @@
 local function run(msg, matches)
     if matches[1]:lower() == '+' and is_sudo(msg) then
         block_user("user#id"..matches[2],ok_cb,false)
-        return "User blocked"
+        return "⚫User Blocked⚫"
     end
     if matches[1]:lower() == '-' and is_sudo(msg) then
 	    unblock_user("user#id"..matches[2],ok_cb,false)
-        return "User unblocked"
+        return "⚪User UnBlocked⚪"
     end
 return {
   patterns = {
